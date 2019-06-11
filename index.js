82a008282bdae8c06ffa0a16b6f0ccd1d1baa9d3
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2019-06-11 19:23:47 -0300

    Criando usuário inicial

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 mongoose.Promise = global.Promise
 const mongo = process.env.MONGODB || 'mongodb://localhost/devnews'
@@ -15,16 +16,23 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-mongoose.connect(mongo, { useNewUrlParser: true }).then(()=>{
+const createInitialUser = async () => {
+    const total = await User.count({ username: 'leonardocn' })
+    if (total === 0) {
+        const user = new User({
+            username: 'leonardocn',
+            password: '123'
+        })
+        await user.save()
+        console.log('Usuário inicial criado')
+    }else{
+        console.log('Criação de usuário inicial ignorada')
+    }
+}
+
+mongoose.connect(mongo, { useNewUrlParser: true }).then(() => {
     app.listen(port, () => {
+        createInitialUser()
         console.log(`Servidor está rodando na porta ${port}`)
     })
 }).catch(err => console.log(err))
-
-
-const User = require('./models/user')
-const user  = new User({
-    username:'leonardocn',
-    password:'123'
-})
-user.save(()=>console.log('save'))
